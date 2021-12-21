2500a192dc525f9640ebf7af9822438b0c21c72d
SylwesterIwanowski <50060453+SylwesterIwanowski@users.noreply.github.com>
2021-12-21 22:33:54 +0100

    add products to list

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ ReactDOM.render(
   <div>
     <h1>My Favorite Foods</h1>
     <ul>
-      <li>Mit</li>
-      <li>Fructis</li>
-      <li>Vegetable</li>
+      <li>meat</li>
+      <li>vegetables</li>
+      <li>fruits</li>
     </ul>
   </div>,
   document.getElementById("root")
